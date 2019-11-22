f2e8b4a20c959eea8c6e90a49d3145665401d598
Christian Cosgrove <ccosgro2@jhu.edu>
2019-11-22 12:42:39 -0500

    synthetic

--- edit_distance_correlation.py
+++ edit_distance_correlation.py
@@ -1,3 +1,12 @@
 # TODO: measure correlations between minhash, weighted jaccard, and OMH with true edit distance
 
 # Make some graphs
+from random import choice
+from string import ascii_uppercase
+
+def gen_string_universe():
+    length_range = 12
+    s = sum(([''.join(choice(ascii_uppercase) for _ in range(l))] for l in range(1, length_range)))
+    # print(''.join(choice(ascii_uppercase) for _ in range(12)))
+
+gen_string_universe()
