b38b6942d8656de120233ea9fa8936e063ac0245
Cindy <xyang73@jhu.edu>
2019-11-24 21:01:49 -0500

    basic generation

--- synthetic_data.py
+++ synthetic_data.py
@@ -1,31 +1,38 @@
+import random
 from random import seed
 from random import randint
 
 
-parent = "GTTGATAAGCAAGCATCTCATTTTGTGCATATACCTGGTCTTTCGTATTCTGGCGTGAAGTCGCCGNCTGAATGCCAGCAATCTCTTTTTGAGTCTCATT"
-length = len(parent)
-depth = 10
+# cparent = "GTTGATAAGCAAGCATCTCATTTTGTGCATATACCTGGTCTTTCGTATTCTGGCGTGAAGTCGCCGNCTGAATGCCAGCAATCTCTTTTTGAGTCTCATT"
+# cparent = "GTTGATAAGCAAGCA"
+cparent = "ATGC"
+# length = len(cparent)
+depth = 2
 base = ['A', 'T', 'G', 'C']
 genome_list = []
+seed(5)
 
 def generate_genome():
 # mutate the parent genome for every child 
 # output a list of genome
 
-child_list = [parent]
-cur_list = []
-genome_list.insert(parent)
+	child_list = []
+	cur_list = [cparent]
+	genome_list.append(cparent)
 
-for i in range(depth):
-	for item in child_list:
-	# generate child for every genome
-		for j in range(randint(2, 8)):
-			pos_to_change = randint(0, length-1)
-			new_child = mutate(pos, item)
-
-			# add new chile to list and trees
-			genome_list.insert(new_child)
-			cur_list.insert(new_child)
+	for i in range(depth):
+		child_list, cur_list = cur_list, []
+		print("*************")
+		for item in child_list:
+			print("-------------")
+			# generate child for every genome
+			for j in range(randint(2, 8)):
+				pos_to_change = randint(0, len(item)-1)
+				new_child = mutate(pos_to_change, item)
+				print(new_child)
+				# append new chile to list and trees
+				genome_list.append(new_child)
+				cur_list.append(new_child)
 
 
 
@@ -45,4 +52,9 @@ def mutate(pos, item):
 			b = random.choice(base)
 		return (item[:pos] + b + item[pos+1:])
 
+
 def generate_graph():
+	return
+
+
+generate_genome()
