e5ec2e914c69b836fdae204b15569df08a5497d0
Shuhao Lai <ShuhaoLai18@gmail.com>
2019-12-05 03:16:16 -0500

    Added .nex bird data and documented how to parse it

--- bird_tree_subtree.py
+++ bird_tree_subtree.py
@@ -1,25 +1,30 @@
 # for reading, writing, and analyzing files--like .nex--related to phylogenic trees
-import dendropy
+from dendropy import Tree, TreeList
 
-def view_phylogenic_tree():
-    print(tree2.as_ascii_plot())
+#################################################################################################################
+'''
+BRIEF BREAKDOWN OF DENDROPY TO GET YOU STARTED:
 
+Our bird phylogenetic tree data has >= 100 trees in the output.nex file. The TreeList class is used to handle
+a list of phylogenetic trees (like in our case) while Tree is used to handle one phylogenetic tree.
+Dendropy accepts .nex (nexus) files among many others.
+
+install dendropy at https://dendropy.org/downloading.html
+'''
+#################################################################################################################
+'''
+Helpful methods for the TREE class from the link https://dendropy.org/library/treemodel.html#dendropy.datamodel.treemodel.Tree
+tree.as_ascii_plot()
+    print tree out in ascii
+tree.ageorder_node_iter(descending=True)
+    iterate through node of the tree from root down
+tree.edges()
+    returns list of edges of tree.
+'''
+#################################################################################################################
 
 if __name__ == '__main__':
-    FILEPATH = "C:\\Users\\Shuha\\Downloads\\tree-pruner-7bb9002c-63e8-4a5d-b6ee-139ae5e0f95d\\output.nex"
-    tree2 = dendropy.Tree.get(file=open(FILEPATH, "r"), schema="nexus")
-    view_phylogenic_tree()
-
-# def parse_bird_tree_download(filepath):
-#     # Strings that mark the end and beginning of .NEX output file from BirdTree.org
-#     BEGIN = "BEGIN TREES;\n"
-#     END = "END;\n"
-#     with open(filepath, 'r') as f:  # open the file
-#         output_list = f.readlines()
-#         start = output_list.index(BEGIN)
-#         end = output_list.index(END)
-#         if end == -1 or start == -1 or end <= start:
-#             raise Exception("No tree exists or file is not in right format.")
-#         subtree = output_list[start+1:end]
-#         for a in subtree:
-#             print(a.strip())
+    FILEPATH = 'C:\\Users\\Shuha\\PycharmProjects\\Computational_Genomics\\genomic_final\\bird_phylogenic_tree.nex'
+    treelist = TreeList.get(path=FILEPATH, schema="nexus")
+    assert len(treelist) == 200
+    print(treelist[0].as_ascii_plot())
